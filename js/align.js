e0b68196fb60fcc0f6aa8fbf85c90eaa797eca63
Jer <jeremiah.n.dunn@gmail.com>
2011-09-14 19:51:51 +0100

    added js to reposition elements on page resize or loading page on small screen

--- js/align.js
+++ js/align.js
@@ -1,12 +1,29 @@
+//min width before resizing doesn't effect position of text
+var w_min = 1000;
+var w_box = $('.announcmentbox').width();
+
 //Alligns elements on page
 $(document).ready(function() {
-		var m = $('.announcmentbox').width();
-		var w = $(window).width();
-		
-		if (m < w){
-				var offset = (w - m)/2;
+		if (w_min < $(window).width()){
+				var offset = ($(window).width() - w_box)/2;
 				offset = offset + 'px';
 				$('.announcmentbox').css('left', offset);
+		}else{
+		  var offset = (w_min - w_box)/2;
+		  offset = offset + 'px';
+				$('.announcmentbox').css('left', offset);
 		}
-  
+});
+
+//rescales page after resize
+$(window).resize(function () {
+		if (w_min < $(window).width()){
+				var offset = ($(window).width() - w_box)/2;
+				offset = offset + 'px';
+				$('.announcmentbox').css('left', offset);
+		}else{
+		  var offset = (w_min - w_box)/2;
+		  offset = offset + 'px';
+				$('.announcmentbox').css('left', offset);
+		}
 });
