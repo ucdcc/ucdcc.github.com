4f8483f89826fb315be3f3062ea8bcf95b9580f4
Diarmuid <diarmuidr3d@gmail.com>
2014-09-02 12:32:31 +0100

    Added committee phone numbers to committee page

--- committee.php
+++ committee.php
@@ -20,62 +20,9 @@
     
 		<!-- menu -->
     
-		<div class="navbar navbar-fixed-top ">
-      
-			<div class="navbar-inner">
-	
-				<div class="center_header">
-	  
-					<ul class="nav ">
-	    
-						<li>
-	      <a href="index.html" class="unselected">Home</a>
-	    </li>
-	    
-						<li>
-	      <a href="newmembers.html" class="unselected">New Members</a>
-	    </li>
-	    
-						<li>
-	      <a href="disciplines.html" class="unselected">Kayaking</a>
-	    </li>
-	    
-						<li>
-	      <a href="events.html" class="unselected">Trips Away</a>
-	    </li>
-	    
-						<li class="active">
-	      <a href="committee.html">Committee &amp; Contact</a>
-	    </li>
-	    
-						<li>
-	      <a href="http://forum.ucdcanoeclub.com" class="unselected">Forum</a>
-	    </li>
-	    
-						<li>
-	      <a href="http://ucdcanoeclub.wordpress.com/" target="_blank" class="unselected">Blog</a>
-	    </li>
-	    
-						<li>	     
-	      <a href="https://www.facebook.com/group.php?gid=7060672244" target="_blank" class="unselected">Facebook</a>
-	    </li>
-	    
-						<li>
-	      <a href="https://twitter.com/#!/ucdcc" target="_blank" class="unselected">Twitter</a>
-	    </li>
-	    
-						<!--<li>
-	      <a href="contact.html">Contact</a>
-	    </li>
--->	  
-					</ul>
-	
-				</div>
-      
-			</div>
-    
-		</div>
-    
+		<?php
+			include 'includes/menu.php';
+		?>   
     
 		<div class ="container top_padding">
     
